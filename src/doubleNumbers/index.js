681288cb1b8c1dfad78b644da1edfe2df3c0781d
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2019-11-06 22:33:16 +0200

    finish

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,39 @@
 /* eslint-disable-next-line */
 export function doubleNum(num) {
-  // your logic here...
+  let length = num.toString().length;
+  if (length % 2 === 0) {
+    let arr = num.toString().split('');
+    let str1 = arr.slice(0, length / 2).join();
+    let str2 = arr.slice(length / 2, length).join();
+    return str1 === str2 ? num : num * 2;
+  }
+
+  return num * 2;
+
 }
+
+
+// function doubleNum (num) {
+//   // your logic here...
+//   const str = num.toString();
+//   const strReverse = str.split('').reverse().join('');
+//   console.log(str)
+//   console.log(strReverse)
+//   // var arr = [1, 2, 3, 1, 2, 3];
+//   // for (var i = 0 )
+//   //console.log(str.slice(-1)[0]);
+//   var flag = true;
+//   for (let i = 0; i < str.length; i++) {
+//     for (let j = strReverse.length - 1; j >= 0; j--) {
+//       if ( str[i] !== strReverse[j]) {
+//         // console.log('str => ', str[i]);
+//         // console.log('strReverse', strReverse[j]);
+//         console.log(str[i], ' === ', strReverse[j]);
+//         flag = false;
+//       }
+//     }
+//   }
+
+//   return flag ? num : num * 2;
+
+// }
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,66 @@
 /* eslint-disable-next-line */
-export function sortByWeight(arr) {
-  // your logic here...
+// export function sortByWeight(arr) {
+//   // var obj = {val: '123', weight: 6};
+//   let newArr = arr.map(el => {
+//     return {
+//       val: el,
+//       weight: el.split('').reduce(function(acc, item) {
+//           return acc + parseInt(item, 10);
+//       }, 0),
+//     };
+//   });
+
+//   newArr.sort(function(a,b) {
+//     if (a.weight > b.weight) {
+//       return 1;
+//     }
+//     return -1;
+//   });
+//   return newArr.map(function(el) {
+//     return el.val;
+//   });
+
+
+//   // console.log(newArr);
+// }
+
+
+export function sortByWeight (arr) {
+  return arr.sort(function (a,b) {
+
+    let x = a.split('').reduce(function (acc, item) {
+      return acc + parseInt(item, 10);
+    }, 0);
+    let y = b.split('').reduce(function (acc, item) {
+      return acc + parseInt(item, 10);
+    }, 0);
+    if (x > y) {
+      return 1;
+    }
+    return -1;
+  });
 }
+
+
+
+
+
+
+// const arr = ['123', '1', '72'];
+// function sortByWeight(arr) {
+//   // your logic here...
+
+//   for (var i = 0; i < arr.length; i++) {
+//     for (var j = 1; j < arr.length; j++) {
+//       if (arr[i] < arr[j]) {
+//         let el = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = el;
+//       }
+//     }
+//   }
+
+//   return  arr;
+// }
+
+console.log(sortByWeight(['123', '1', '72']));
